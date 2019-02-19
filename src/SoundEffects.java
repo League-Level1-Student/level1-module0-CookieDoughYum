f69c4b03f469d6e0df273a25f44b23aa8e6a49d4
CookieDoughYum <league@WTS13.attlocal.net>
2019-02-18 16:56:44 -0800

    Finished sound effects and cuteness tv and working on spinning record

--- src/SoundEffects.java
+++ src/SoundEffects.java
@@ -1,4 +1,49 @@
+import java.applet.AudioClip;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SoundEffects {
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SoundEffects implements ActionListener{
+	JButton button=new JButton();
+	JButton button1=new JButton();
+	JButton button2=new JButton();
+	JFrame frame=new JFrame();
+	JPanel panel=new JPanel();
+public static void main(String[] args) {
+	SoundEffects se= new SoundEffects();
+}
+public SoundEffects() {
+	frame.add(panel);
+	panel.add(button);
+	panel.add(button1);
+	panel.add(button2);
+	button.addActionListener(this);
+	button1.addActionListener(this);
+	button2.addActionListener(this);
+	frame.pack();
+	frame.setVisible(true);
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	if(e.getSource()==button) {
+		playSound("Button.wav");
+	}
+	if(e.getSource()==button1) {
+		playSound("Button1.wav");
+	}
+	if(e.getSource()==button2) {
+		playSound("Button3.wav");
+	}
+}
+private void playSound(String fileName) {
+    AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName)); 
+    sound.play();
+}
+
 
 }
---
+++ src/SpinningRecord/SpinningRecord.pde
@@ -0,0 +1,23 @@
+import ddf.minim.*;
+PImage pictureOfRecord;
+  //at the very top of your sketch
+Minim minim;  //as a member variable
+AudioPlayer song;  //as a member variable
+int spin=0;
+void setup(){   
+  size(600,600);  
+  pictureOfRecord= loadImage("record.jpg");  
+  pictureOfRecord.resize(400,400);
+}
+void draw(){
+  rotateImage(pictureOfRecord, spin);
+  if(mousePressed){
+  image(pictureOfRecord, 100, 100); 
+  }
+  spin+=10;
+}
+void rotateImage(PImage image, int amountToRotate) {
+     translate(width/2, height/2);
+     rotate(amountToRotate*TWO_PI/360);
+     translate(-image.width/2, -image.height/2);
+}
